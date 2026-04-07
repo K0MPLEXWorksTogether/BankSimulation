6b56b6b7999e8825533477623d3355309a33d343
K0MPLEXWorksTogether <abhirammangipudi03@gmail.com>
2026-04-07 12:45:54 +0530

    [feat] Repositories for Models

--- src/main/java/tech/abhirammangipudi/interfaces/Authenticate.java
+++ src/main/java/tech/abhirammangipudi/interfaces/Authenticate.java
@@ -1,8 +1,9 @@
 package tech.abhirammangipudi.interfaces;
 
 import tech.abhirammangipudi.errors.AuthenticationException;
-import tech.abhirammangipudi.models.User;
 
-public interface Authenticate {
-    boolean authenticate(User user, String password) throws AuthenticationException;
+public interface Authenticate<T> {
+    T login(String username, String password) throws AuthenticationException;
+
+    void logout(T entity);
 }
